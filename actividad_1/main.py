4dbab52bdc3286d266b1017096e212876f7a91b6
Hans patiño <hanks.patino64@gmail.com>
2025-08-16 20:57:50 -0500

    utilizando los seters

--- actividad_1/main.py
+++ actividad_1/main.py
@@ -2,3 +2,6 @@ from clases.animal import*
 
 A = animal("luky",7.45,"hans","29/04/06","28/09/25")
 print("El nombre del tu animal es: ", A.nombre," y el propietario es: ",A.propietario)
+
+A.fechaCumple =input("ingrese la nueva fecha: ")
+print("nueva fecha: ", A.fechaCumple) 
